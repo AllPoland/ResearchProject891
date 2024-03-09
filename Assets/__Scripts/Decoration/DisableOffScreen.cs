c3dec67f9bc7090972517cde5d1376fdad851de5
AllPoland <adff.gaming@gmail.com>
2024-03-09 15:38:36 -0700

    improve moon music
    only using the lowest register sounded really bad

---
+++ Assets/__Scripts/Decoration/DisableOffScreen.cs
@@ -0,0 +1,43 @@
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DisableOffScreen : MonoBehaviour
+{
+    [SerializeField] private MeshRenderer[] contributors;
+    [SerializeField] private bool startPrimed = false;
+    [SerializeField] public UnityEvent OnTriggered;
+
+    private bool visible;
+    private bool primed;
+
+
+    public void Prime()
+    {
+        primed = true;
+    }
+
+
+    private void OnEnable()
+    {
+        if(startPrimed)
+        {
+            Prime();
+        }
+
+        visible = false;
+    }
+
+
+    private void Update()
+    {
+        bool newVisible = contributors.Any(x => x.isVisible);
+        if(primed && visible && !newVisible)
+        {
+            OnTriggered?.Invoke();
+            primed = false;
+        }
+
+        visible = newVisible;
+    }
+}
