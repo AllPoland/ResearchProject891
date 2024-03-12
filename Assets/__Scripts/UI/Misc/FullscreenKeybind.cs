4c1ce66349026bfa244ec2d1e94c4eff0b660b20
AllPoland <adff.gaming@gmail.com>
2024-03-11 21:36:04 -0600

    fix fullscreen keybind

--- Assets/__Scripts/UI/Misc/FullscreenKeybind.cs
+++ Assets/__Scripts/UI/Misc/FullscreenKeybind.cs
@@ -3,19 +3,25 @@ using UnityEngine;
 public class FullscreenKeybind : MonoBehaviour
 {
 #if !UNITY_WEBGL || UNITY_EDITOR
+    private int preferredWindowWidth = Screen.width;
+    private int preferredWindowHeight = Screen.height;
+
     private void Update()
     {
         if(Input.GetButtonDown("ToggleFullscreen"))
         {
-            if(Screen.fullScreenMode != FullScreenMode.MaximizedWindow)
+            if(Screen.fullScreen)
             {
-                Debug.Log("Fullscreen enabled.");
-                Screen.fullScreenMode = FullScreenMode.MaximizedWindow;
+                Debug.Log("Fullscreen disabled.");
+                Screen.SetResolution(preferredWindowWidth, preferredWindowHeight, FullScreenMode.Windowed);
             }
             else
             {
-                Debug.Log("Fullscreen disabled.");
-                Screen.fullScreenMode = FullScreenMode.Windowed;
+                Debug.Log("Fullscreen enabled.");
+                preferredWindowWidth = Screen.width;
+                preferredWindowHeight = Screen.height;
+
+                Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, FullScreenMode.FullScreenWindow);
             }
         }
     }
